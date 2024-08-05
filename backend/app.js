e7fc1eb6976838c3f7a146be61f274d7778dc694
Lev Potapov <potapov.lev1996@gmail.com>
2024-08-05 08:38:01 +0200

    The backend structure and all models for the database have been created, all necessary connections have been made.

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const booksRoutes = require("./routes/books");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Admin:Azerty123123@cluster0.pcvcj4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Successful connection to MongoDB !"))
+  .catch(() => console.log("Connection to MongoDB failed !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", booksRoutes);
+
+module.exports = app;
+
+//MongoDB LG = Admin PW = Azerty123123
